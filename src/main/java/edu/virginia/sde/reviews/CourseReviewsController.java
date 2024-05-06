8b20e696423bdf0e5dcc6776c2797f419b918df1
Chris Goodhart <Chris@Chriss-MacBook-Air.local>
2024-05-06 17:09:31 -0400

    Made basic functionality for review table, made test main method

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
@@ -4,20 +4,46 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseReviewsController {
     private DatabaseDriver databaseDriver;
     private Stage stage;
     private Course course;
+    @FXML
     private Label courseLabel;
+    @FXML
+    private TableView<Review> reviewTable;
+    @FXML
+    private TableColumn<Review, String> timeColumn;
+    @FXML
+    private TableColumn<Review, Integer> ratingColumn;
+    @FXML
+    private TableColumn<Review, String> commentColumn;
     public void setStage(Stage stage, Course course) {
         this.stage = stage;
         this.course = course;
         String courseInfo = String.format("%s %d: %s", course.getSubject(), course.getCourseNumber(), course.getTitle());
         courseLabel.setText(courseInfo);
+
+        populateReviewTable();
+    }
+
+    private void populateReviewTable(){
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("timestamp"));
+        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        commentColumn.setCellValueFactory(new PropertyValueFactory<>("comment"));
+
+        List<Review> reviews = course.getReviews();
+        reviewTable.getItems().addAll(reviews);
     }
 
     @FXML
---
+++ src/main/java/edu/virginia/sde/reviews/MainTest.java
@@ -0,0 +1,40 @@
+package edu.virginia.sde.reviews;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Timestamp;
+
+public class MainTest extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("coursereviews.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        int courseNumber = 3140;
+        String subject = "CS";
+        String title = "Software Development Essentials";
+        List<Review> reviews = new ArrayList<>();
+        Course sde = new Course(courseNumber, subject, title, reviews);
+
+
+        Review r1 = new Review(1, 5, new Timestamp(300000), "Good class", "student", sde);
+        reviews.add(r1);
+        sde.setReviews(reviews);
+
+
+        CourseReviewsController courseReviewsController = fxmlLoader.getController();
+        courseReviewsController.setStage(stage, sde);
+        stage.setTitle("Course Review");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
