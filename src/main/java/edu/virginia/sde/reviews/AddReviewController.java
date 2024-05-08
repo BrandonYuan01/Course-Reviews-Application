b755bf876e5c548b18bf3e566a2c3120f4ce4392
Chris Goodhart <Chris@Chriss-MacBook-Air.local>
2024-05-08 15:02:17 -0400

    Small bug fixes, added basic input checks for submit button

--- src/main/java/edu/virginia/sde/reviews/AddReviewController.java
+++ src/main/java/edu/virginia/sde/reviews/AddReviewController.java
@@ -4,13 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 // todo list
-// function for back button
-// update header with class info
 // function for submit button
 
 public class AddReviewController {
@@ -20,6 +20,12 @@ public class AddReviewController {
     private DatabaseDriver databaseDriver;
     @FXML
     private Label courseLabel;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private TextField ratingField;
+    @FXML
+    private TextArea commentArea;
 
     public void setStage(Stage stage, Course course, String username) {
         this.stage = stage;
@@ -43,5 +49,19 @@ public class AddReviewController {
         CourseReviewsController courseReviewsController = fxmlLoader.getController();
         courseReviewsController.setStage(stage, course, username);
         stage.setScene(scene);
+    }
+
+    @FXML
+    public void submit() {
+        String rating = ratingField.getText();
+        if (rating.isEmpty()){
+            errorLabel.setText("Rating cannot be empty.");
+        }
+        else if (!rating.matches("[1-5]")){
+            errorLabel.setText("Rating must be an integer from 1 to 5.");
+        }
+        else{
+            errorLabel.setText("Good job!");
+        }
     }
 }
